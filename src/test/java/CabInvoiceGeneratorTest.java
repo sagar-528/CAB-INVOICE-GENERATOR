5f4e5b0bdf569e7c0bf8e2f9b90b7d115c99b3f6
sagar-528 <gupta.sagar528@gmail.com>
2020-04-16 16:59:21 +0530

    ADDED: Code to calculate fare using distance and time

---
+++ src/test/java/CabInvoiceGeneratorTest.java
@@ -0,0 +1,15 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CabInvoiceGeneratorTest {
+
+    @Test
+    public void whenGivenDistanceAndTime_ShouldReturnTotalFare() {
+
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 2.0;
+        int time = 5;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(25, fare, 0.0);
+    }
+}
