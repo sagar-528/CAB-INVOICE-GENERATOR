6e8767c1a5f739d4a1bc0a894f3e0ce0b81a5d14
sagar-528 <gupta.sagar528@gmail.com>
2020-04-16 17:08:06 +0530

    ADDED: Code to return minimum fare given less time or distance

--- src/main/java/InvoiceGenerator.java
+++ src/main/java/InvoiceGenerator.java
@@ -2,8 +2,10 @@ public class InvoiceGenerator {
 
     private static final double MINIMUM_COST_PER_KILOMETER = 10;
     private static final int COST_PER_MINUTE = 1;
+    private static final double MINIMUM_FARE = 5;
 
     public double calculateFare(double distance, int time) {
-        return distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_MINUTE;
+        double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_MINUTE;
+        return (totalFare < MINIMUM_FARE) ? MINIMUM_FARE : totalFare;
     }
 }
--- src/test/java/CabInvoiceGeneratorTest.java
+++ src/test/java/CabInvoiceGeneratorTest.java
@@ -5,11 +5,19 @@ public class CabInvoiceGeneratorTest {
 
     @Test
     public void whenGivenDistanceAndTime_ShouldReturnTotalFare() {
-
         InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         double distance = 2.0;
         int time = 5;
         double fare = invoiceGenerator.calculateFare(distance, time);
         Assert.assertEquals(25, fare, 0.0);
+    }
+
+    @Test
+    public void givenDistanceAndTime_ShouldReturnMinimumFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 0.1;
+        int time = 1;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(5, fare, 0.0);
     }
 }
