dd234960f167cf0fb3f9b6e6cfb3ac59ad0efea3
sagar-528 <gupta.sagar528@gmail.com>
2020-04-17 00:48:38 +0530

    ADDED: Invoice Summary use Case

--- src/main/java/InvoiceService.java
+++ src/main/java/InvoiceService.java
@@ -1,8 +1,13 @@
-public class InvoiceGenerator {
+public class InvoiceService {
 
     private static final double MINIMUM_COST_PER_KILOMETER = 10;
     private static final int COST_PER_MINUTE = 1;
     private static final double MINIMUM_FARE = 5;
+    private RideRepository rideRepository;
+
+    public InvoiceService() {
+        this.rideRepository = new RideRepository();
+    }
 
     public double calculateFare(double distance, int time) {
         double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_MINUTE;
@@ -15,5 +20,13 @@ public class InvoiceGenerator {
             totalFare += this.calculateFare(ride.distance, ride.time);
         }
         return new InvoiceSummary(rides.length, totalFare);
+    }
+
+    public void addRides(String userId, Ride[] rides){
+        rideRepository.addRides(userId, rides);
+    }
+
+    public InvoiceSummary getInvoiceSummary(String userId){
+        return this.calculateFare(rideRepository.getrides(userId));
     }
 }
--- src/test/java/InvoiceServiceTest.java
+++ src/test/java/InvoiceServiceTest.java
@@ -1,21 +1,20 @@
-import com.sun.media.sound.RIFFInvalidDataException;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-public class CabInvoiceGeneratorTest {
-    InvoiceGenerator invoiceGenerator = null;
+public class InvoiceServiceTest {
+    InvoiceService invoiceService = null;
 
     @Before
     public void setUp() throws Exception{
-        invoiceGenerator = new InvoiceGenerator();
+        invoiceService = new InvoiceService();
     }
 
     @Test
     public void whenGivenDistanceAndTime_ShouldReturnTotalFare() {
         double distance = 2.0;
         int time = 5;
-        double fare = invoiceGenerator.calculateFare(distance, time);
+        double fare = invoiceService.calculateFare(distance, time);
         Assert.assertEquals(25, fare, 0.0);
     }
 
@@ -23,7 +22,7 @@ public class CabInvoiceGeneratorTest {
     public void givenDistanceAndTime_ShouldReturnMinimumFare() {
         double distance = 0.1;
         int time = 1;
-        double fare = invoiceGenerator.calculateFare(distance, time);
+        double fare = invoiceService.calculateFare(distance, time);
         Assert.assertEquals(5, fare, 0.0);
     }
 
@@ -33,7 +32,20 @@ public class CabInvoiceGeneratorTest {
               new Ride(2.0, 5),
               new Ride(0.1, 1)
         };
-        InvoiceSummary summary = invoiceGenerator.calculateFare(rides);
+        InvoiceSummary summary = invoiceService.calculateFare(rides);
+        InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 30.0);
+        Assert.assertEquals(expectedInvoiceSummary, summary);
+    }
+
+    @Test
+    public void givenUserIdAndRides_ShouldReturnInvoice() {
+        String userId = "A@T.com";
+        Ride[] rides = {
+                new Ride(2.0, 5),
+                new Ride(0.1, 1)
+        };
+        invoiceService.addRides(userId, rides);
+        InvoiceSummary summary = invoiceService.getInvoiceSummary(userId);
         InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 30.0);
         Assert.assertEquals(expectedInvoiceSummary, summary);
     }
